fixed bug with different units

ChromMovie.py
@@ -157,7 +157,7 @@ class MD_simulation:
                 frames = self.get_frames_positions_npy()
                 points = np.vstack(tuple(frames))
                 cif_path = self.output_path+f"/struct_{str(i+1).zfill(2)}_res{resolution2text(res)}_ready.cif"
-                write_mmcif(points, cif_path)
+                write_mmcif(points*10, cif_path)
                 
 
     def simulate_resolution(self, resolution: int, sim_step: int, frame_path_npy: str, frame_path_cif: str, params: dict) -> None:
@@ -232,7 +232,7 @@ class MD_simulation:
         # Saving new starting point:
         points = np.vstack(tuple(frames_new))
         cif_path = self.output_path+f"/struct_{str(index).zfill(2)}_res{resolution2text(new_res)}_init.cif"
-        write_mmcif(points, cif_path)
+        write_mmcif(points*10, cif_path)
 
         # Updating parameters:
         self.m = new_m
@@ -243,7 +243,7 @@ class MD_simulation:
             forcefield = ForceField('forcefields/classic_sm_ff.xml')
             self.system = forcefield.createSystem(cif.topology, nonbondedCutoff=1*u.nanometer)
             integrator = mm.LangevinIntegrator(310, 0.05, 100 * mm.unit.femtosecond)
-
+            
             # Add forces
             self.adjust_force_params(new_res)
             params = self.force_params.copy()

ChromMovie_utils.py
@@ -143,7 +143,7 @@ def extrapolate_points(points: np.array, n: int) -> np.array:
             p = (p1[0] * (1 - alpha) + p2[0] * alpha, p1[1] * (1 - alpha) + p2[1] * alpha,
                  p1[2] * (1 - alpha) + p2[2] * alpha)
             points_new.append(p)
-    return points_new
+    return np.array(points_new)
 
 
 def get_custom_force_formula(f_type: str="attractive", f_formula: str="harmonic", linear: bool=False, l_bound: float=1, u_bound: float=1, u_linear:float=1) -> str: